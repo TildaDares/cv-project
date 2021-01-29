484f353309deae36a55cb8c771f6aa74d3a50a7b
Tilda Udufo <mathildaudufo@gmail.com>
2021-01-29 22:18:30 +0100

    Changed Resume component to functional component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,17 +1,14 @@
-import React from "react";
 import Paper from "@material-ui/core/Paper";
-import General from "./General"
+import General from "./General";
 import "../styles/main.css";
-class Resume extends React.Component {
-	render() {
-		return (
-			<Paper elevation={3} className="paper">
-				<form noValidate className="form" autoComplete="off">
-          <General/>
-				</form>
-			</Paper>
-		);
-	}
+function Resume(props) {
+	return (
+		<Paper elevation={3} className="paper">
+			<form noValidate className="form" autoComplete="off">
+				<General />
+			</form>
+		</Paper>
+	);
 }
 
 export default Resume;
