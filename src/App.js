06e2bc799408bfdd7d3593f39e42c8008fbaf145
Tilda Udufo <mathildaudufo@gmail.com>
2021-01-29 17:42:54 +0100

    Changed App functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Resume from './components/Resume';
+import React from "react";
+import Resume from "./components/Resume";
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/ToolBar";
 import Typography from "@material-ui/core/Typography";
@@ -7,27 +8,30 @@ import VisibilityIcon from "@material-ui/icons/Visibility";
 import logo from "../logo.png";
 import "../styles/navbar.css";
 import "../styles/main.css";
-function App() {
-  return (
-		<div>
-			<AppBar color="default">
-				<ToolBar className="d-flex justify-around">
-					<div className="d-flex">
-						<img src={logo} alt="logo" className="main-logo" />
-						<Typography variant="h6">CV Maker</Typography>
-					</div>
-					<Button
-						color="primary"
-						variant="contained"
-						startIcon={<VisibilityIcon />}
-					>
-						Preview
-					</Button>
-				</ToolBar>
-			</AppBar>
-			<Resume />
-		</div>
-	);
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<AppBar color="default">
+					<ToolBar className="d-flex justify-around">
+						<div className="d-flex">
+							<img src={logo} alt="logo" className="main-logo" />
+							<Typography variant="h6">CV Maker</Typography>
+						</div>
+						<Button
+							color="primary"
+							variant="contained"
+							startIcon={<VisibilityIcon />}
+						>
+							Preview
+						</Button>
+					</ToolBar>
+				</AppBar>
+				<Resume />
+			</div>
+		);
+	}
 }
 
 export default App;
