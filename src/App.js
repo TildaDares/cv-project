fc1f05342fa2210bf8e71951d8137a46d3fa828b
Tilda Udufo <mathildaudufo@gmail.com>
2021-01-29 20:16:48 +0100

    Moved NavBar Component from App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 import Resume from "./components/Resume";
-import AppBar from "@material-ui/core/AppBar";
-import ToolBar from "@material-ui/core/ToolBar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import VisibilityIcon from "@material-ui/icons/Visibility";
-import logo from "../logo.png";
-import "../styles/navbar.css";
 import "../styles/main.css";
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<AppBar color="default">
-					<ToolBar className="d-flex justify-around">
-						<div className="d-flex">
-							<img src={logo} alt="logo" className="main-logo" />
-							<Typography variant="h6">CV Maker</Typography>
-						</div>
-						<Button
-							color="primary"
-							variant="contained"
-							startIcon={<VisibilityIcon />}
-						>
-							Preview
-						</Button>
-					</ToolBar>
-				</AppBar>
+				<NavBar/>
 				<Resume />
 			</div>
 		);
