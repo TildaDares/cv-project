4677e3d7cf80cb4f05d37613d642c521a6306e8a
Tilda Udufo <mathildaudufo@gmail.com>
2021-01-30 22:38:11 +0100

    Changed Education to a class component

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,22 +1,32 @@
+import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
 import EducationForm from "./EducationForm";
 import "../../styles/education.css";
 
-export default function Education(props) {
-	return (
-		<div className="cv-sections">
-			<Typography variant="h3" className="heading">
-				Education
-			</Typography>
-			<Divider variant="middle" className="dividers" />
-			<div className="justify-center">
-				<EducationForm />
+export default class Education extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editForm: false
+    }
+  }
+  
+	render() {
+		return (
+			<div className="cv-sections">
+				<Typography variant="h3" className="heading">
+					Education
+				</Typography>
+				<Divider variant="middle" className="dividers" />
+				<div className="justify-center">
+					<EducationForm />
+				</div>
+				<Button color="primary" className="float-right" variant="contained">
+					Add Education
+				</Button>
 			</div>
-			<Button color="primary" className="float-right" variant="contained">
-				Add Education
-			</Button>
-		</div>
-	);
+		);
+	}
 }
