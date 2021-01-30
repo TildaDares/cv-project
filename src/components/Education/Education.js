dd906734aa7a65c2be8f823182c575e1c2733fc1
Tilda Udufo <mathildaudufo@gmail.com>
2021-01-30 22:13:32 +0100

    Done wit the education form

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,19 +1,22 @@
-import Typography from "@material-ui/core/Typography"
+import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
-import '../../styles/education.css';
+import EducationForm from "./EducationForm";
+import "../../styles/education.css";
 
 export default function Education(props) {
-  return (
+	return (
 		<div class="cv-sections">
 			<Typography variant="h3" className="heading">
 				Education
 			</Typography>
 			<Divider variant="middle" className="dividers" />
-      <div className="justify-center">
-
-      </div>
-      <Button color="primary" className="float-right" variant="contained">Add Education</Button>
+			<div className="justify-center">
+				<EducationForm />
+			</div>
+			<Button color="primary" className="float-right" variant="contained">
+				Add Education
+			</Button>
 		</div>
 	);
 }
---
+++ src/components/Education/EducationForm.js
@@ -0,0 +1,18 @@
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+export default function EducationForm() {
+	return (
+		<form className="cv-forms">
+			<TextField label="School" type="text" required />
+			<TextField label="Degree" type="text" required />
+			<TextField label="Field of Study" type="text" required />
+			<div className="d-flex dates">
+				<TextField label="Start Date" type="month" required />
+				<TextField label="End Date" type="month" required />
+			</div>
+			<Button variant="contained" color="secondary" className="submit" type="submit">
+				Submit
+			</Button>
+		</form>
+	);
+}
