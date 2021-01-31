948986babca7431eab0b4e3e192d27226b64fea0
Tilda Udufo <mathildaudufo@gmail.com>
2021-01-31 01:09:12 +0100

    Handled EducationFormState

--- src/components/Education/EducationForm.js
+++ src/components/Education/EducationForm.js
@@ -1,36 +1,113 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-export default class EducationForm extends React.Component{
-  render() {
-    return (
-			<form className="cv-forms">
-				<TextField label="School" type="text" required />
-				<TextField label="Degree" type="text" required />
-				<TextField label="Field of Study" type="text" required />
+export default class EducationForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			school: "",
+			degree: "",
+			study: "",
+			startDate: "",
+			endDate: "",
+			startDateHelperText: "",
+			startDateError: false,
+		};
+		this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleErrors = this.handleErrors.bind(this);
+	}
+
+	handleSubmit(event) {
+    event.preventDefault();
+    if(!this.handleErrors()) {
+      console.log("Errors are about");
+    }
+  }
+
+	handleErrors() {
+		const fields = ["school", "degree", "study", "startDate", "endDate"];
+		fields.forEach((field) => {
+			if (this.state[field] === "") {
+				return false;
+			}
+		});
+		if (this.state.startDate > this.state.endDate) {
+			this.setState({
+				startDateHelperText: "Start date cannot be lesser",
+				startDateError: true,
+			});
+			return false;
+		} else {
+			this.setState({
+				startDateHelperText: "",
+				startDateError: false,
+			});
+		}
+		return true;
+	}
+
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	}
+
+	render() {
+		return (
+			<form className="cv-forms" onSubmit={this.handleSubmit}>
+				<TextField
+					label="School"
+					name="school"
+					type="text"
+					onChange={this.handleChange}
+					required
+				/>
+				<TextField
+					label="Degree"
+					name="degree"
+					type="text"
+					onChange={this.handleChange}
+					required
+				/>
+				<TextField
+					label="Field of Study"
+					name="study"
+					type="text"
+					onChange={this.handleChange}
+					required
+				/>
 				<div className="d-flex dates">
-					<TextField label="Start Date" type="month" required />
+					<TextField
+            error={this.state.startDateError}
+						label="Start Date"
+						name="startDate"
+            type="month"
+            helperText={this.state.startDateHelperText}
+						onChange={this.handleChange}
+						required
+					/>
 					<TextField
 						label="End Date"
 						type="month"
+						name="endDate"
 						helperText="Or expected graduation"
+						onChange={this.handleChange}
 						required
 					/>
 				</div>
-				<div className="d-flex justify-between">
+				<div className="d-flex justify-between footer-btns">
 					<Button
 						variant="contained"
 						color="secondary"
-						className="submit"
 						type="button"
-						onClick={props.handleEducationChange}
+						onClick={this.props.handleEducationChange}
 					>
 						Cancel
 					</Button>
 					<Button
 						variant="contained"
 						color="primary"
-						className="submit"
 						type="submit"
 					>
 						Submit
@@ -38,5 +115,5 @@ export default class EducationForm extends React.Component{
 				</div>
 			</form>
 		);
-  }
+	}
 }
