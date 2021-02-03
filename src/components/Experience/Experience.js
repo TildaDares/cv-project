e1529ba2d01e0f52f0dddbee46140896b96b1d5b
Tilda Udufo <mathildaudufo@gmail.com>
2021-02-03 22:32:35 +0100

    Added Add Experience button

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import "../../styles/education.css";
 export default class Experience extends React.Component {
 	constructor(props) {
 		super(props);
@@ -10,13 +13,22 @@ export default class Experience extends React.Component {
 	}
 	render() {
 		return (
-			<Button
-				color="primary"
-				className="float-right add-btn"
-				startIcon={<AddIcon />}
-			>
-				Add Education
-			</Button>
+			<div className="cv-sections">
+				<Typography variant="h3" className="heading">
+					Experience
+				</Typography>
+				<Divider variant="middle" className="dividers" />
+				<div className="cv-section">
+					<div className="container"></div>
+				</div>
+				<Button
+					color="primary"
+					className="float-right add-btn"
+					startIcon={<AddIcon />}
+				>
+					Add Experience
+				</Button>
+			</div>
 		);
 	}
 }
