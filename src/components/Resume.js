e7ba96bb225f1188b578365aecd080e19c1e43ce
Tilda Udufo <mathildaudufo@gmail.com>
2021-02-07 19:25:57 +0100

    passed isReadOnly as prop to Skills

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,6 +2,7 @@ import Paper from "@material-ui/core/Paper";
 import General from "./General";
 import Education from "./Education/Education";
 import Experience from "./Experience/Experience";
+import Skills from "./Skills";
 import "../styles/main.css";
 function Resume(props) {
 	return (
@@ -9,6 +10,7 @@ function Resume(props) {
 			<General isReadOnly={props.isReadOnly} />
 			<Education isReadOnly={props.isReadOnly} />
 			<Experience isReadOnly={props.isReadOnly} />
+			<Skills isReadOnly={props.isReadOnly} />
 		</Paper>
 	);
 }
